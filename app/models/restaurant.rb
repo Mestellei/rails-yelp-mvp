6b23d89990a0b4f1df51b82e8a5f60f046ce5a57
Marie Estelle IOrio <marieestelle.iorio@gmail.com>
2019-05-20 15:45:20 +0200

    modifié

---
+++ app/models/restaurant.rb
@@ -0,0 +1,6 @@
+class Restaurant < ApplicationRecord
+  CATEGORIES = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+puts "deleting restaurants"
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Chez Vong',
+    address:      '4 rue des Marthyrs, 75009 Paris',
+    phone_number: '01 45 45 16 60',
+    category:     'chinese',
+  },
+  {
+    name:         'Hokkaido',
+    address:      '4 rue des Marthyrs, 75009 Paris',
+    phone_number: '01 45 45 16 60',
+    category:     'japanese',
+  },
+    {
+    name:         'L epicure',
+    address:      '4 rue des Marthyrs, 75009 Paris',
+    phone_number: '01 45 45 16 60',
+    category:     'french',
+  },
+  {
+    name:         'Parseni',
+    address:      '4 rue des Marthyrs, 75009 Paris',
+    phone_number: '01 45 45 16 60',
+    category:     'italian',
+  },
+    {
+    name:         'Soon grill',
+    address:      '4 rue des Marthyrs, 75009 Paris',
+    phone_number: '01 45 45 16 60',
+    category:     'chinese',
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts "finished"
